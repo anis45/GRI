6f2974426a8c42f7e4f48110ed1e0986f15e73cb
meziani <anis.meziani45@gmail.com>
2021-02-12 14:35:24 +0100

    nombre de triangle

--- TP3/tp3.py
+++ TP3/tp3.py
@@ -4,36 +4,22 @@ from TP1 import *
 
 
 def nb_triangle(graph,sommet):
-	print()
-
+	compte = 0
+	voisin = graph.neighbors(sommet)
+	for v in voisin:
+		for v_2 in graph.neighbors(v):
+			if v_2 in voisin:
+				compte += 1
+	compte = int(compte/2)
+	print(compte)
+	return compte
 
 if __name__ == "__main__":
 	triangle = sys.argv[1]
 	filename = sys.argv[2]
-	nb_arc = sys.argv[3]
-	sommet = sys.argv[4]
-	edg = read_edges(fname, m_estim)
+	nb_arc = int(sys.argv[3])
+	sommet = int(sys.argv[4])
+	edg = read_edges(filename, nb_arc)
 	g = Graph(edg, symmetrize=True)
-
-    # fname = sys.argv[1]
-    # m_estim = int(sys.argv[2])
-    # mem()
-    # edg = read_edges(fname, m_estim)
-    # mem()
-    # g = Graph(edg, symmetrize=True)
-    # print(f"n={g.n}")
-    # print(f"m={g.m}")
-    # mem()
-    # degmax = 0
-    # for u in g.nodes() :
-    #     du = len(g.neighbors(u))
-    #     if du > degmax :
-    #         degmax = du
-    # print(f"degmax={degmax}")
-    # mem()
-    # trav = Traversal(g.n)
-    # src = int(sys.argv[3])
-    # dst = int(sys.argv[4])
-    # for i in range(1) : trav.bfs(g, src+i) # 10s per BFS on soc-pokec
-    # print(f"dist={trav.dist[dst]}")
-    # mem()
+	nb_triangle(g,sommet)
+    
